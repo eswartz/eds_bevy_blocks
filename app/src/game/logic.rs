21faa30370b163ef10cf8dc65d85b7af29e21f83
Ed Swartz <875407+eswartz@users.noreply.github.com>
2026-03-20 19:43:46 -0500

    Use HandStatusArea for on-screen indicator of fire power.

--- app/src/game/logic.rs
+++ app/src/game/logic.rs
@@ -23,6 +23,12 @@ impl Plugin for LogicPlugin {
             .add_plugins(HighlightingPlugin)
             .add_plugins(GrabbingPlugin)
             .insert_resource(HighlightingMode::Enabled)
+            .init_resource::<FirePower>()
+            .insert_resource(FirePowerStats {
+                accel: 1.1,
+                max: 100.0,
+                start: 0.1,
+            })
             .add_systems(
                 FixedUpdate,
                 (
@@ -54,6 +60,27 @@ impl Plugin for LogicPlugin {
         ;
     }
 }
+
+#[derive(Resource, Default, Debug, Deref, DerefMut, Reflect)]
+#[reflect(Resource)]
+pub struct FirePower(pub f32);
+
+#[derive(Resource, Debug, Reflect)]
+#[reflect(Resource)]
+pub struct FirePowerStats {
+    pub accel: f32,
+    pub start: f32,
+    pub max: f32,
+}
+
+impl FirePowerStats {
+    pub(crate) fn apply_force(&self, dt: Duration, power: f32) -> f32 {
+        let q = (dt.as_secs_f32() * 64.0).min(1.0);
+        let mul = 1.0.lerp(self.accel, q);
+        (power * mul).min(self.max)
+    }
+}
+
 
 pub(crate) fn play_player_out_of_bounds(
     mut commands: Commands,
@@ -87,7 +114,9 @@ fn check_actions(
     actions: Res<ActionState<UserAction>>,
     player_q: Query<(Entity, &Transform, &ColliderAabb), With<Player>>,
     player_look_q: Query<&PlayerLook>,
-    mut fire_power: Local<f32>,
+    mut fire_power: ResMut<FirePower>,
+    fire_power_stats: Res<FirePowerStats>,
+    time: Res<Time>,
 ) {
     // Only one player...
     let Ok((player, player_xfrm, aabb)) = player_q.single() else {
@@ -100,19 +129,19 @@ fn check_actions(
     };
 
     if actions.just_pressed(&UserAction::Fire) {
-        *fire_power = 0.1;
+        **fire_power = fire_power_stats.start;
     }
     else if actions.pressed(&UserAction::Fire) {
-        *fire_power = (*fire_power * 1.25).min(50.0);
+        **fire_power = fire_power_stats.apply_force(time.delta(), **fire_power);
     }
-    if actions.just_released(&UserAction::Fire) && *fire_power > 0. {
+    if actions.just_released(&UserAction::Fire) && **fire_power > 0. {
         // Fire something.
         commands.write_message(FireProjectile {
             look_xfrm: Transform::from_translation(player_xfrm.translation).with_rotation(look.rotation),
-            power: *fire_power,
+            power: **fire_power,
         });
 
-        *fire_power = 0.;
+        **fire_power = 0.;
     }
 }
 
@@ -129,10 +158,12 @@ fn check_actions(
 
     exist_q: Query<Entity>,
     fx: Res<CommonFxAssets>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+    meshes: ResMut<Assets<Mesh>>,
 
-    mut fire_power: Local<f32>,
+    mut fire_power: ResMut<FirePower>,
+    fire_power_stats: Res<FirePowerStats>,
+    time: Res<Time>,
 ) {
     // Only one player...
     let Ok((player, player_xfrm, aabb)) = player_q.single() else {
@@ -149,73 +180,20 @@ fn check_actions(
 
     let fire = fire_events.iter().next().unwrap();
     if fire.contains(ActionEvents::START) {
-        *fire_power = 0.1;
+        **fire_power = fire_power_stats.start;
     }
     else if fire.contains(ActionEvents::FIRE) {
-        *fire_power = (*fire_power * 1.25).min(50.0);
+        **fire_power = fire_power_stats.apply_force(time.delta(), **fire_power);
     }
-    else if fire.contains(ActionEvents::COMPLETE) && *fire_power > 0. {
+    else if fire.contains(ActionEvents::COMPLETE) && **fire_power > 0. {
         // Fire something.
 
         let xfrm = Transform::from_translation(position).with_rotation(look.rotation);
-        let power = *fire_power;
+        let power = **fire_power;
 
         do_fire(commands.reborrow(), xfrm, power, grabbed_opt, exist_q, fx, materials, meshes);
-        // let vel = xfrm.rotation * Vec3::NEG_Z * power;
-        // let mut any = false;
-        // if let Some(grabbed) = &grabbed_opt {
-        //     // Fire the item we are holding, if it still exists.
-        //     if exist_q.contains(grabbed.entity) {
-        //         commands.queue(WakeBody(grabbed.entity));
-        //         commands.entity(grabbed.entity).insert((
-        //             LinearVelocity(vel),
-        //         ));
-        //         commands.write_message(GrabbingCommand::ReleaseItems);
-        //         any = true;
-        //     } else {
-        //         commands.write_message(GrabbingCommand::CancelGrabItems);
-        //     }
-        //     // commands.remove_resource::<GrabbedItem>();
-        // } else {
-        //     // Fire a new item.
-        //     let mat = materials.add(Color::srgba(0.7, 0.2, 0.2, 1.1));
-        //     let size = Vec3::new(2.0, 0.5, 0.5);
-        //     let mesh = meshes.add(Cuboid::from_size(size));
 
-        //     commands.spawn(((
-        //         Name::new("BOOM"),
-        //         Mesh3d(mesh.clone()),
-        //         MeshMaterial3d(mat.clone()),
-        //         xfrm,
-        //         DespawnAfter(Duration::from_secs(120)),
-        //     ), (
-        //         Spawned,
-        //         Projectile,
-        //         CrosshairTargetable,
-        //         CollisionEventsEnabled,
-        //         LinearVelocity(vel),
-        //         Mass(250.0),
-        //         Friction::new(0.25),
-        //         Restitution::new(0.5),
-        //         SweptCcd::default(),
-        //         RigidBody::Dynamic,
-        //         Collider::cuboid(size.x, size.y, size.z),
-        //     )));
-        //     any = true;
-        // }
-
-        // if any {
-        //     commands.spawn((
-        //         UiSfx,
-        //         SamplePlayer::new(fx.swoosh.clone()),
-        //     ));
-        // }
-        // // commands.write_message(FireProjectile {
-        // //     look_xfrm: Transform::from_translation(position).with_rotation(look.rotation),
-        // //     power: *fire_power,
-        // // });
-
-        *fire_power = 0.;
+        **fire_power = 0.;
     }
 }
 
@@ -233,21 +211,6 @@ fn do_fire(
     mut meshes: ResMut<Assets<Mesh>>,
 
 ) -> bool {
-    // // Only one player...
-    // let Ok((player, player_xfrm, aabb)) = player_q.single() else {
-    //     log::error!("no single Player");
-    //     return;
-    // };
-    // let Ok(look) = player_look_q.get(player) else {
-    //     log::error!("no PlayerLook");
-    //     return;
-    // };
-
-    // let eyes = player_eyes(player_xfrm, aabb, look);
-    // let position = player_gun(&look.rotation, eyes);
-
-        // let xfrm = Transform::from_translation(position).with_rotation(look.rotation);
-
     let vel = xfrm.rotation * Vec3::NEG_Z * power;
     let mut any = false;
     if let Some(grabbed) = &grabbed_opt {
@@ -297,14 +260,9 @@ fn do_fire(
             SamplePlayer::new(fx.swoosh.clone()),
         ));
     }
-    // commands.write_message(FireProjectile {
-    //     look_xfrm: Transform::from_translation(position).with_rotation(look.rotation),
-    //     power: *fire_power,
-    // });
 
     any
 }
-
 
 fn report_raycast(
     mut info_q: Single<(&mut Text, &mut TextColor, &mut Visibility), With<InfoArea>>,
