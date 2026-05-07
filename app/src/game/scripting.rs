f40af6c7efaa820a800eea562d09f35b7a1887b2
Ed Swartz <875407+eswartz@users.noreply.github.com>
2026-05-07 17:34:55 -0500

    Adapt to translating MidiSynthMessage into `write_message`

--- app/src/game/scripting.rs
+++ app/src/game/scripting.rs
@@ -1,14 +1,8 @@
-use crate::game::bevy_funcs::add_script;
-use crate::game::bevy_funcs::add_velocity;
-use crate::game::bevy_funcs::remove_script;
-use crate::game::bevy_funcs::send_midi_message;
-use crate::game::bevy_funcs::set_gravity;
-use crate::game::bevy_funcs::spawn_cube;
-use crate::game::bevy_funcs::translate;
+use crate::game::bevy_funcs::*;
 
 use bevy::prelude::*;
 
-use fedry_bevy_plugin::bevy_world_function_service::BevyWorldFunctionService;
+use fedry_bevy_plugin::bevy_system_service::BevySystemService;
 use fedry_bevy_plugin::prelude::*;
 
 pub struct ScriptingPlugin;
@@ -26,26 +20,30 @@ impl Plugin for ScriptingPlugin {
 
 fn register_funcs(mut commands: Commands, runtime: Res<ScriptRuntime>) {
     let rt = &runtime.rt;
-    let bevy_world_service = rt.get_service::<BevyWorldFunctionService>().expect("expected BevyWorldService");
-    bevy_world_service.add_world_func(
+    let bevy_system_service = rt.get_service::<BevySystemService>().expect("expected BevySystemService");
+
+    bevy_system_service.add_runtime_system(
         rt.pool.for_str("spawn_cube"),
         commands.register_system(spawn_cube));
-    bevy_world_service.add_world_func(
+    bevy_system_service.add_runtime_system(
         rt.pool.for_str("send_midi_message"),
         commands.register_system(send_midi_message));
-    bevy_world_service.add_world_func(
-        rt.pool.for_str("add_script"),
-        commands.register_system(add_script));
-    bevy_world_service.add_world_func(
-        rt.pool.for_str("remove_script"),
-        commands.register_system(remove_script));
-    bevy_world_service.add_world_func(
+
+    bevy_system_service.add_runtime_system(
         rt.pool.for_str("translate"),
         commands.register_system(translate));
-    bevy_world_service.add_world_func(
+    bevy_system_service.add_runtime_system(
         rt.pool.for_str("add_velocity"),
         commands.register_system(add_velocity));
-    bevy_world_service.add_world_func(
+    bevy_system_service.add_runtime_system(
         rt.pool.for_str("set_gravity"),
         commands.register_system(set_gravity));
+
+    bevy_system_service.add_runtime_system(
+        rt.pool.for_str("add_script"),
+        commands.register_system(add_script));
+    bevy_system_service.add_runtime_system(
+        rt.pool.for_str("remove_script"),
+        commands.register_system(remove_script));
+
 }
