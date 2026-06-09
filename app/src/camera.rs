a87e2269a1ef89f7d7caf6f0a4700b3ff19e30e4
Ed Swartz <875407+eswartz@users.noreply.github.com>
2026-06-09 15:24:28 -0500

    Make flashlight point down.

--- app/src/camera.rs
+++ app/src/camera.rs
@@ -123,7 +123,7 @@ fn configure_world_camera(mut ent_commands: EntityCommands, use_clustered: bool)
             children![
                 (
                     Name::new("LIGHT"),
-                    Transform::from_xyz(0.0, 0.5, -1.0),
+                    Transform::from_xyz(0.0, 0.5, -1.0).rotate_x(-std::f32::consts::FRAC_PI_4),
                     Flashlight::default(),
                 )
             ],
